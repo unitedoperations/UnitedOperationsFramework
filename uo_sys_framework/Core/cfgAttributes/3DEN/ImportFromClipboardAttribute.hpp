cc061e934be491fd462e04c07b54aec2afc635db
PiZZAD0X <bravoecholimalima@gmail.com>
2019-01-31 19:48:33 -0800

    Unneeded parentheses

--- uo_sys_framework/Core/cfgAttributes/3DEN/ImportFromClipboardAttribute.hpp
+++ uo_sys_framework/Core/cfgAttributes/3DEN/ImportFromClipboardAttribute.hpp
@@ -3,7 +3,7 @@ class UO_FW_ImportFromClipboardAttribute: Title {
         class Title: Title {};
         class Value: ctrlButtonPictureKeepAspect {
             idc=100;
-            x="(48) * (pixelW * pixelGrid * 0.50)";
+            x="48 * (pixelW * pixelGrid * 0.50)";
             w="0.99 * 5 * (pixelW * pixelGrid * 0.50)";
             h="5 * (pixelH * pixelGrid * 0.50)";
             text="\a3\3DEN\Data\Attributes\ComboPreview\play_ca.paa";
