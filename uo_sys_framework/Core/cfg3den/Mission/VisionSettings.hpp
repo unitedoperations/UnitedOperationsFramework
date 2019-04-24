93f2ab91398cd94e9d9bc00adaafc6a9967bf03f
Christian Sacher <reyfin@gmx.de>
2019-04-24 21:28:28 +0200

    Fix naming

--- uo_sys_framework/Core/cfg3den/Mission/VisionSettings.hpp
+++ uo_sys_framework/Core/cfg3den/Mission/VisionSettings.hpp
@@ -4,16 +4,16 @@ class EGVAR(Core,ViewDistanceSettings) {
     class Attributes {
         class EGVAR(Core,VisionPlayerEnabled) {
             property = QEGVAR(Core,VisionPlayerEnabled);
-            displayName = "Player Viewdistance";
-            tooltip = "Enable Player Viewdistance";
+            displayName = "Player View-distance";
+            tooltip = "Enable Player View-distance";
             control = "Checkbox";
             expression = SCENARIO_EXPRESSION;
             defaultValue = "false";
         };
         class EGVAR(Core,PlayerViewDistance) {
             property = QEGVAR(Core,PlayerViewDistance);
-            displayName = "Player View Distance";
-            tooltip = "Set the Player View Distance in meters. Players might not see all the way to the limit due to fog but AI can.";
+            displayName = "Player View-distance";
+            tooltip = "Set the Player View-distance in meters. Players might not see all the way to the limit due to fog but AI can.";
             control = QMGVAR(200To10000Step50_Slider);
             expression = SCENARIO_EXPRESSION;
             validate = "number";
@@ -30,8 +30,8 @@ class EGVAR(Core,ViewDistanceSettings) {
         };
         class EGVAR(Core,AIViewDistance) {
             property = QEGVAR(Core,AIViewDistance);
-            displayName = "AI View Distance";
-            tooltip = "Set the AI View Distance in meters.";
+            displayName = "AI View-distance";
+            tooltip = "Set the AI View-distance in meters.";
             control = QMGVAR(200To10000Step50_Slider);
             expression = SCENARIO_EXPRESSION;
             validate = "number";
